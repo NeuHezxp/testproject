5004a9c9beda967345139311a1668e429c2568d8
NeuHezxp <mpatino@student.neumont.edu>
2023-11-06 19:01:47 -0700

    pushing testing conflicts

--- testproject/tester.cpp
+++ testproject/tester.cpp
@@ -5,8 +5,8 @@ int main(int argc, char* argv[])
 {
 	std::cout << "Hello World" << std::endl;
 	std::cout << "Hello World" << std::endl;
+	std::cout << "Hello changed" << std::endl;
 	std::cout << "Hello World" << std::endl;
-	std::cout << "Hello World" << std::endl;
-	std::cout << "Hello World" << std::endl;
+	std::cout << "wack World" << std::endl;
 	return 0;
 }
