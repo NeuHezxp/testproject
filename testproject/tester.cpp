4a82b4f7369255f31628bba5098b52bd291a63e3
NeuHezxp <mpatino@student.neumont.edu>
2023-11-06 19:14:09 -0700

    testing branches

--- testproject/tester.cpp
+++ testproject/tester.cpp
@@ -8,6 +8,6 @@ int main(int argc, char* argv[])
 	std::cout << "Hello changed" << std::endl;
 	std::cout << "Hello World" << std::endl;
 	std::cout << "Hello World" << std::endl;
-	std::cout << "Hello World" << std::endl;
+	std::cout << "Hello" << std::endl;
 	return 0;
 }
